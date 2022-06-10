cc8d8205059c8488dcc100c8072145c89e22d5de
Nathan Center <nathanjcenter@gmail.com>
2022-06-10 10:17:59 -0500

    first commit

---
+++ bankTest/BankAccountTestsBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bankTest
+{
+    [TestClass]
+    public class BankAccountTestsBase
+    {
+
+        [TestMethod]
+        public void BankAccount()
+        {
+        }
+    }
+}
